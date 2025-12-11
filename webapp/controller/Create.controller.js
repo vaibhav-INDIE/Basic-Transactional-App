4c1bbed3c35049307c87e06960b9ea3df611c4f6
Vaibhav Jain <info.airassoi@gmail.com>
2025-12-11 06:31:40 +0000

    Adding Odata service for CRUD operations

--- webapp/controller/Create.controller.js
+++ webapp/controller/Create.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+  "sap/m/MessageToast",
+  "sap/m/MessageBox"
+], function (Controller, MessageToast, MessageBox) {
   "use strict";
 
   return Controller.extend("demo.productapp.productapp.controller.Create", {
@@ -15,8 +16,9 @@ sap.ui.define([
     },
 
     onSave: function () {
-      var oView = this.getView();
-      var oModel = this.getOwnerComponent().getModel("data");
+      var oView  = this.getView();
+      // Default model = OData V4 model for z_ui_products
+      var oModel = oView.getModel();
 
       var sName  = oView.byId("demo.productapp.productapp::Create--InName").getValue();
       var sPrice = oView.byId("demo.productapp.productapp::Create--InPrice").getValue();
@@ -27,21 +29,35 @@ sap.ui.define([
         return;
       }
 
-      var aProducts = oModel.getProperty("/products");
-      var iNextId = oModel.getProperty("/nextId");
+      // Generate a ProductID (max length 10)
+      // You can change this logic to whatever your backend expects.
+      var sProductID = sName.toUpperCase().replace(/\s+/g, "_").slice(0, 10);
+      if (!sProductID) {
+        sProductID = String(Date.now()).slice(-10);
+      }
 
-      aProducts.push({
-        id: iNextId,
-        name: sName,
-        price: sPrice ? Number(sPrice) : 0,
-        stock: sStock ? Number(sStock) : 0
-      });
+      // Build payload exactly as in metadata
+      var oNewProduct = {
+        ProductID: sProductID,
+        Name:      sName,
+        Price:     sPrice ? Number(sPrice) : 0,
+        Currency:  "INR",          // default; change if needed
+        Category:  "General",      // default; change/add input if needed
+        Stock:     sStock ? Number(sStock) : 0
+      };
 
-      oModel.setProperty("/products", aProducts);
-      oModel.setProperty("/nextId", iNextId + 1);
+      // OData V4 create on entity set /Products
+      var oListBinding = oModel.bindList("/Products");
 
-      MessageToast.show("Product created");
-      this._getRouter().navTo("List");
+      oListBinding.create(oNewProduct)
+        .created()
+        .then(function () {
+          MessageToast.show("Product created");
+          this._getRouter().navTo("List");
+        }.bind(this))
+        .catch(function (oError) {
+          MessageBox.error(oError.message || "Error while creating product");
+        });
     }
 
   });
